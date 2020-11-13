27b20d30b70cfaf3c08510208d393c7c0bdfdad2
Kavin Valli <kavinvalli@gmail.com>
2020-11-13 15:40:02 +0530

    some changes

--- lib/main.dart
+++ lib/main.dart
@@ -17,35 +17,23 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   List<Todo> testTodos = [
     Todo(title: "Go to market", description: "Buy vegetables", isDone: false),
-    Todo(title: "Homework", description: "Maths Assignment", isDone: false),
+    Todo(title: "Homework", description: "Maths Assignment which is very very very very very very very very very very very very very", isDone: false),
     Todo(title: "Programming", description: "Posting API", isDone: false),
   ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
 //      appBar: AppBar(title: Text("Todo App"),),
-      backgroundColor: Color(0xFFd6d6dc),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(
-            height: 30,
-          ),
-          Text(
-              "Todos",
-              style: TextStyle(
-                fontSize: 30,
-              ),
-          ),
-          Container(
-            child: FutureBuilder<List<Todo>>(
-              future: DBProvider.db.getAllTodos(),
-              builder: (BuildContext context, AsyncSnapshot<List<Todo>> snapshot) {
-                if (snapshot.hasData) {
-                  return ListView.builder(
-                    itemCount: snapshot.data.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      Todo item = snapshot.data[index];
+//      backgroundColor: Color(0xFFd6d6dc),
+      body: SafeArea(
+        child: FutureBuilder<List<Todo>>(
+          future: DBProvider.db.getAllTodos(),
+          builder: (BuildContext context, AsyncSnapshot<List<Todo>> snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                itemCount: snapshot.data.length,
+                itemBuilder: (BuildContext context, int index) {
+                  Todo item = snapshot.data[index];
 //                return ListTile(
 //                  title: Text(item.title),
 //                  leading: Text(item.id.toString()),
@@ -57,33 +45,53 @@ class _MyAppState extends State<MyApp> {
 //                    value: item.isDone,
 //                  ),
 //                );
-                      return Container(
-                          margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 15.0),
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                  return Container(
+                      margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 15.0),
+                      padding: EdgeInsets.symmetric(vertical: 2.0, horizontal: 0.0),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                      ),
+                      child: Row(
+                        children: <Widget>[
+                          Checkbox(
+                            onChanged: (bool value) {
+                              DBProvider.db.doneOrUndone(item);
+                              setState(() {});
+                            },
+                            value: item.isDone,
                           ),
-                          child: Row(
-                            children: <Widget>[
-                              Checkbox(
-                                onChanged: (bool value) {
-                                  DBProvider.db.doneOrUndone(item);
-                                  setState(() {});
-                                },
-                                value: item.isDone,
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                  item.title,
+                                style: TextStyle(
+                                  fontSize: 16.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 4.0,
+                              ),
+                              Text(
+                                  item.description,
+                                  overflow: TextOverflow.ellipsis,
+                                  maxLines: 1,
+                                  softWrap: false,
                               )
                             ],
-                          )
-                      );
-                    },
+                          ),
+                        ],
+                      )
                   );
-                } else {
-                  return Center(child: CircularProgressIndicator(),);
-                }
-              },
-            ),
-          ),
-        ],
+                },
+              );
+            } else {
+              return Center(child: CircularProgressIndicator(),);
+            }
+          },
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
